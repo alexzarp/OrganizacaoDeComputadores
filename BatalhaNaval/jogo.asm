b9ae054f0e5e210df27f3f03dfbddc8116d49142
alexzarp <alexszds@gmail.com>
2021-08-26 11:51:48 -0300

    Co-authored-by: Bruna Gabriela Disner <bruna.disner@gmail.com>

--- BatalhaNaval/jogo.asm
+++ BatalhaNaval/jogo.asm
@@ -10,6 +10,7 @@ invalida_fora:      .string     "Posição inválida por estar fora da matriz"
 invalida_sobreposto:  .string    "Posição inválida por estar sobreescrevendo navio existente"
 invalida_n:          .string     "Posição inválida pois o navio é maior que a matriz"
 br_n:       .string     "\n"
+space:      .string     " "
     .text
 main:
     jal insere_embarcacoes
@@ -47,17 +48,18 @@ insere_embarcacoes:
     continua_ins:
     add t1, zero, zero # para navios diposicao horizontal
     addi t2, zero, 1 # para navios disposição vertical
+
     la a2, matriz # a2 navios
     lw a3, (a2) # matriz onde vai os navios
     lb a4, (a1) # string com a descricao dos navios
     add t0, a4, zero # contagem de navios
     addi a1, a1, 2
-    lb a4, (a1) # string com a descricao dos navios
     teste_condicao_ins:
         beq t0, zero, fim_ins
     corpo_laco_ins:
-        add a5, a4, zero
-        
+        lb a4, (a1) # string com a descricao dos navios
+        add a5, a4, zero 
+
         addi a1, a1, 2
         lb a4, (a1) # string com a descricao dos navios
         addi t3, a4, 0 # comprimento do navio
@@ -70,7 +72,7 @@ insere_embarcacoes:
         # Deslocamento = (L * QTD_colunas + C) * 4
         addi t6, zero, 9
         addi s3, zero, 4
-        addi s4, zero, 0
+        # addi s4, zero, 0
         mul s0, t4, t6 # multiplicação da linha por 9(número de colunas da matriz)
         add s1, s0, t5 # soma de s0 com a coluna
         mul s0, s1, s3 # resultado final do deslocamento 
@@ -98,12 +100,12 @@ insere_embarcacoes:
             vertical_ins:
                 addi a2, a2, 40 # mesmo que 4 * 10 posições
             continua_ins_h:
-                j corpo_laco_ins
+                j corpo_laco_ins_h
         fim_ins_h:
             addi s11, s11, 1
-            j incremento_controle_ins
+            j teste_condicao_ins_h
 
-    incremento_controle_ins:
+    teste_condicao_ins_h:
         addi t0, t0, -1
         addi a1, a1, 2
         la a2, matriz
@@ -116,9 +118,90 @@ insere_embarcacoes:
 #     add s8, t3, zero
 #     teste_condicao_val:
 #         beq 
+altera: # função cedida pelo Jeferson da dupla Jeferson Krumenauer e Vinícius Todescato
+    addi t1,zero,48
+    beq a4,t1,troca48
+    addi t1,zero,49
+    beq a4,t1,troca49
+    addi t1,zero,50
+    beq a4,t1,troca50
+    addi t1,zero,51
+    beq a4,t1,troca51
+    addi t1,zero,52
+    beq a4,t1,troca52
+    addi t1,zero,53
+    beq a4,t1,troca53
+    addi t1,zero,54
+    beq a4,t1,troca54
+    addi t1,zero,55
+    beq a4,t1,troca55
+    addi t1,zero,56
+    beq a4,t1,troca56
+    addi t1,zero,57
+    beq a4,t1,troca57
+    
+    troca48:
+        addi a4,zero,0
+        ret
+    troca49:
+        addi a4,zero,1
+        ret
+    troca50:
+        addi a4,zero,2
+        ret
+    troca51:
+        addi a4,zero,3
+        ret
+    troca52:
+        addi a4,zero,4
+        ret
+    troca53:
+        addi a4,zero,5
+        ret
+    troca54:
+        addi a4,zero,6
+        ret
+    troca55:
+        addi a4,zero,7
+        ret
+    troca56:
+        addi a4,zero,8
+        ret
+    troca57:
+        addi a4,zero,9
+        ret
 
-# printa_matriz:
-#     la 
+printa_matriz:
+    add t0, zero, zero # quando chegar em 100, termina
+    addi t1, zero, 100 
+    add t2, zero, zero # a cada 10, um \n
+    la a1, matriz
+
+    teste_condicao_prin:
+        beq t0, t1, fim_prin
+        lw a2, (a1)
+        
+    
+    corpo_laco_prin:
+        
+        la a0, a2
+        li a7, 1
+        ecall
+
+        la a0, space
+        li a7, 4
+        ecall
+        j incremento_prin
+    incremento_prin:
+        addi t2, t2, 1
+        j corpo_laco_prin
+    incremento_controle_prin:
+        addi a1, a1, 4
+        addi t0, t0, 1
+        j teste_condicao_prin
+    fim_prin:
+
+
 quebra: # só pra dar quebra de linha de forma mais limpa = \n
     la a0, br_n
     li a7, 4
