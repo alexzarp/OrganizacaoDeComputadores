cbae3acdbced1a2be416add5416c9df2ea8c06ab
alexzarp <alexszds@gmail.com>
2021-09-01 18:12:37 -0300

    update

--- BatalhaNaval/jogo.asm
+++ BatalhaNaval/jogo.asm
@@ -8,7 +8,7 @@ navio:      .string     "ABCDEFGHIJK"
 msg_1:      .string     "Escolha o conjunto de posicionamento dos navios (1, 2 ou 3): "
 invalida_fora:      .string     "Posição inválida por estar fora da matriz"
 invalida_sobreposto:  .string    "Posição inválida por estar sobreescrevendo navio existente"
-invalida_maior:          .string     "Posição inválida pois o navio é maior que a matriz"
+invalida_maior:       .string     "Posição inválida pois o navio é maior que a matriz"
 br_n:       .string     "\n"
 space:      .string     " "
     .text
@@ -54,9 +54,6 @@ insere_embarcacoes:
     lw a3, (a2) # matriz onde vai os navios
     lb a4, (a1) # string com a descricao dos navios
 
-    # add s10, zero, ra
-    # jal altera
-    # add ra, zero, s10
     addi a4, a4, -48
     add t0, a4, zero # contagem de navios
 
@@ -66,26 +63,17 @@ insere_embarcacoes:
         beq t0, zero, fim_ins
     corpo_laco_ins:
         lb a4, (a1) # string com a descricao dos navios
-        # add s10, zero, ra
-        # jal altera
-        # add ra, zero, s10
         addi a4, a4, -48
         add a5, a4, zero # disposicao do navio
         
         addi a1, a1, 2
         lb a4, (a1) # string com a descricao dos navios
-        # add s10, zero, ra
-        # jal altera
-        # add ra, zero, s10
         addi a4, a4, -48
         addi t3, a4, 0 # comprimento do navio
 
 
         addi a1, a1, 2
         lb a4, (a1) # string com a descricao dos navios
-        # add s10, zero, ra
-        # jal altera
-        # add ra, zero, s10
         addi a4, a4, -48
         addi t4, a4, 0 # linha do navio
         addi a1, a1, 1
@@ -94,9 +82,6 @@ insere_embarcacoes:
 
         addi a1, a1, 1
         lb a4, (a1)
-        # add s10, zero, ra
-        # jal altera
-        # add ra, zero, s10
         addi a4, a4, -48
         addi t5, a4, 0 # coluna do navio | t0 = contagem de navios, t3 = comprimento do navio, t4 = linha, t5 = coluna
         addi a1, a1, 1
@@ -189,58 +174,6 @@ insere_embarcacoes:
         add s10, zero, ra
         jal zera_matriz
         add ra, zero, s10
-        ret
-
-altera: # função cedida pelo Jeferson da dupla Jeferson Krumenauer e Vinícius Todescato
-    addi a6,zero,48
-    beq a4,a6,troca48
-    addi a6,zero,49
-    beq a4,a6,troca49
-    addi a6,zero,50
-    beq a4,a6,troca50
-    addi a6,zero,51
-    beq a4,a6,troca51
-    addi a6,zero,52
-    beq a4,a6,troca52
-    addi a6,zero,53
-    beq a4,a6,troca53
-    addi a6,zero,54
-    beq a4,a6,troca54
-    addi a6,zero,55
-    beq a4,a6,troca55
-    addi a6,zero,56
-    beq a4,a6,troca56
-    addi a6,zero,57
-    beq a4,a6,troca57
-    troca48:
-        addi a4,zero,0
-        ret
-    troca49:
-        addi a4,zero,1
-        ret
-    troca50:
-        addi a4,zero,2
-        ret
-    troca51:
-        addi a4,zero,3
-        ret
-    troca52:
-        addi a4,zero,4
-        ret
-    troca53:
-        addi a4,zero,5
-        ret
-    troca54:
-        addi a4,zero,6
-        ret
-    troca55:
-        addi a4,zero,7
-        ret
-    troca56:
-        addi a4,zero,8
-        ret
-    troca57:
-        addi a4,zero,9
         ret
 
 printa_matriz:
