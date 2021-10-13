e2819af3360caaa58036d42b797a5e231d771f1e
alexzarp <alexszds@gmail.com>
2021-10-13 11:14:20 -0300

    Co-authored-by: Bruna Gabriela Disner <bruna.disner@gmail.com>

--- MemoriaCache/main.py
+++ MemoriaCache/main.py
@@ -67,6 +67,16 @@ class BlocoMemoria(object):
     def getCelula(self):
         return self.__celula
 
+class ConjuntoCache(object):
+    def __init__(self):
+        self.__conjunto = []
+    
+    def getConjunto(self):
+        return self.__conjunto
+    
+    def addConjunto(self, conjunto):
+        self.__conjunto.append(conjunto)
+
 class LinhaCache(object):
     def __init__(self):
         self.__rotulo = '00000'
@@ -87,18 +97,14 @@ class LinhaCache(object):
         return self.___bloco
         
     def setBloco(self, bloco):
-        self.__bloco = bloco
-        #self.__bloco.append(bloco)
+        # self.__bloco = bloco
+        self.__bloco = []
+        self.__bloco.append(bloco)
     
     def setRotulo(self, rotulo):
         if len(rotulo) <= 5:
             self.__rotulo = rotulo
         self.__completaBits(opcao = 'rotulo')
-    
-    # def setFifo(self, fifo):
-    #     if len(fifo) <= 3:
-    #         self.__fifo = fifo
-    #     self.__completaBits(opcao = 'fifo')
 
     def incrementaFifo(self):
         fifo = int(self.__fifo, 2)
@@ -106,9 +112,6 @@ class LinhaCache(object):
         fifo = str(format(fifo, "b"))
         self.__fifo = fifo
         self.__completaBits(opcao = 'fifo')
-
-    # def zeraFifo(self):
-    #     self.__fifo = '000'
     
     def setValido(self, valido):
         self.__valido = valido
@@ -130,21 +133,32 @@ class LinhaCache(object):
                     bits = bits + '0'
                 self.__rotulo = bits + self.__fifo
 
-def incrementaFifo(cache, blocoDestino):
-    bloco = []
-    bloco.append(cache[blocoDestino].setBloco(bloco))
-    # for i in range(4): # sempre será 4, mas enfim
-    print(bloco)
+def incrementaFifo(cache, conjuntoDestino):
+    conjunto = []
+    conjunto.append(cache[conjuntoDestino].getConjunto())
+    for linha in range(len(conjunto)):
+        linha = []
+        linha.append(conjunto[])
+        conjunto[linha].incrementaFifo()
+        print(conjunto[linha].getFifo())
         
-
+# 2 conjuntos
+# 4 linhas dentro do conjunto
+# 1 bloco dentro da linha
+# 4 celulas dentro do bloco
 def insereCache(cache):
-    for bloco_cache in range(2):
-        linha = LinhaCache()
-        bloco = BlocoMemoria()
+    for conjunto_cache in range(2):
+        conjunto = ConjuntoCache()
         for linha_cache in range(4):
-            bloco.addCelula(CelulaMemoria())
-        linha.setBloco(bloco)
-        cache.append(linha) 
+            linha = LinhaCache()
+            bloco = BlocoMemoria()
+            for celula_memoria in range(4):
+                celula = CelulaMemoria()
+                bloco.addCelula(celula)
+            bloco.addCelula(celula)
+            linha.setBloco(bloco)
+            conjunto.addConjunto(linha)
+        cache.append(conjunto)
 
     return cache
 
@@ -174,8 +188,5 @@ def encheMemoria(memoria):
 memoria = []
 cache = []
 memoria = encheMemoria(memoria)
-print(memoria)
 cache = insereCache(cache)
-# ler conteudo de um endereço de memoria
-# escrever em um determinado endereco de memoria
 incrementaFifo(cache, 0)
